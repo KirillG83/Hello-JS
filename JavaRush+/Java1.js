3c7b604f3f14cb732f77cc6af7235aaa2cab672c
Kirill <97805871+KirillG83@users.noreply.github.com>
2022-08-20 12:38:51 +0300

    Add files via upload

---
+++ JavaRush+/Java1.js
@@ -0,0 +1,8 @@
+let userName = 'Kirill';
+console.log(userName);
+
+const yearofBirth = 1994;
+console.log(yearofBirth);
+
+/*----------------------------------*/
+
---
+++ JavaRush+/Java2.java
@@ -0,0 +1,12 @@
+//--Почему то ругается на class в консоли
+public class Solution {
+    public static void main(String[] args) {
+        int numderOne = 3;
+        int numberTwo = 1;
+        int nuberThree = 1;
+        String string = "00";
+
+        String result = numberThree + string + (numderOne + numberTwo + nuberThree) + string;
+        System.out.println("Разгоняем код на ") + result + "%";
+    }
+}
